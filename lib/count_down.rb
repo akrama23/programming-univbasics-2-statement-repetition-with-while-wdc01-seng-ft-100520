307530bcc8eb6210e89222d985820e58847bf78e
akam alam <arundelfootball23@gmail.com>
2020-09-19 02:53:01 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,8 @@
-
+happy_new_number = 0
 count = 10 
 
-while count <= 10 && 
-  puts count - 1 
+while count <= 10 && count != happy_new_number
+ puts "#{count}"
+  count = count - 1
+  happy_new_nummber = puts "Happy New Year"
   
